0f8d4aec7e0ab2e59ee6ac8ebf0742faf436a09d
linht <linhtk2511044@gmail.com>
2025-03-15 13:34:33 +0700

    fix

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -144,8 +144,6 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'EXCEPTION_HANDLER': 'app.exception.custom_exception_handler',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',

6ed0f7451a3b2dab14dd023892505940caa7c2f0
linht <linhtk2511044@gmail.com>
2025-03-15 13:40:18 +0700

    fix

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -204,7 +204,14 @@ CORS_ALLOW_METHODS = (
     "POST",
     "PUT",
 )
-
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
